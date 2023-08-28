452d68c6339416294061cf78202d86b8c53d03e4
aseng2 <98761137+aseng2@users.noreply.github.com>
2023-08-27 17:06:49 -0700

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Anthony Seng
+# CPSC 386-02
+# 2023-02-16
+# aseng6825@csu.fullerton.edu
+# @aseng2
+#
+# Lab 02-00
+#
+# Black Jack Main program
+#
+"""This is the main program to run Blackjack"""
+from blackjackgame import game
+
+if __name__ == "__main__":
+    game.blackjack().run()
